4fb46735f4a2f164e375c0fdfa9540a0a4a41d39
Eugene Viktarovich <eugene.viktorovich@jcatalog.com>
2016-02-08 10:36:24 +0300

    Fixed export.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import { I18nManager } from 'jcatalog-i18n';
 import FormattedMessage from './components/FormattedMessage.jsx';
 import FormattedHTMLMessage from './components/FormattedHTMLMessage.jsx';
-import packageJson from '../package.json';
 
-export default {
+export {
   I18nManager,
   FormattedMessage,
   FormattedHTMLMessage,
-  version: packageJson.version,
 };
