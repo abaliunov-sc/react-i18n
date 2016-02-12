d7d4cc37b17d546a4c680dbc6135b51524935920
Eugene Viktarovich <eugene.viktorovich@jcatalog.com>
2016-02-12 16:34:28 +0300

    Fixed import.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { I18nManager } from 'jcatalog-i18n';
-import FormattedMessage from 'src/app/components/FormattedMessage.jsx';
-import FormattedHTMLMessage from 'src/app/components/FormattedHTMLMessage.jsx';
+import FormattedMessage from './src/app/components/FormattedMessage.jsx';
+import FormattedHTMLMessage from './src/app/components/FormattedHTMLMessage.jsx';
 
 export {
   I18nManager, // todo: I think it's necessary to remove this export.
