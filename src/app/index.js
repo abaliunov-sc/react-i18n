1f8d3ad9506f85ddc1b5ef48f3746c7b017476ae
Dmitry Divin <dmitry.divin@jcatalog.com>
2016-04-21 14:01:26 +0300

    Fixing dependency

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 import { I18nManager } from 'jcatalog-i18n';
-import FormattedMessage from './components/FormattedMessage.jsx';
-import FormattedHTMLMessage from './components/FormattedHTMLMessage.jsx';
+import FormattedMessage from './components/FormattedMessage';
+import FormattedHTMLMessage from './components/FormattedHTMLMessage';
 
 export {
   I18nManager, // todo: I think it's necessary to remove this export.
