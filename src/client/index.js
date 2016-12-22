992c591426b86c1c115853b60d2183b5185c2af7
kirillvolkovich <kirill.volkovich@jcatalog.com>
2016-12-22 15:43:25 +0300

    Fixed package bundle exports

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,11 @@
 import { I18nManager } from 'opuscapita-i18n';
-import FormattedMessage from './components/FormattedMessage.react';
-import FormattedHTMLMessage from './components/FormattedHTMLMessage.react';
+import I18nContext from './components/I18Context/I18nContext.react.js';
+import FormattedMessage from './components/FormattedMessage/FormattedMessage.react';
+import FormattedHTMLMessage from './components/FormattedHTMLMessage/FormattedHTMLMessage.react';
 
 export {
   I18nManager, // todo: I think it's necessary to remove this export.
+  I18nContext,
   FormattedMessage,
   FormattedHTMLMessage,
 };
