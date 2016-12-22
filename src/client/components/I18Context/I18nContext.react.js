2b49945a9b48ed432abba160e1fa34d71139ea33
kirillvolkovich <kirill.volkovich@jcatalog.com>
2016-12-22 15:26:18 +0300

    Implemented `I18nContext` component
    Use it as a parent component if you need pass I18nManager to children context.
    Instance of I18nManager exposes as context.i18n.
    
    Now it has no support of I18nManager.register(). It should be implemented soon.

---
+++ src/client/components/I18Context/I18nContext.react.js
@@ -0,0 +1,59 @@
+import React, { Component, PropTypes } from 'react';
+import I18nManager from 'opuscapita-i18n/lib/utils/I18nManager';
+import defaultProps from './defaultProps';
+
+class I18nContext extends Component {
+  constructor(props) {
+    super(props);
+    this.initManager(props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.initManager(nextProps);
+  }
+
+  initManager(props) {
+    let {
+      locale,
+      intlDatas,
+      formatInfos,
+      defaultLocale
+    } = props;
+    this.i18n = new I18nManager(locale, intlDatas, formatInfos, defaultLocale);
+  }
+
+  getChildContext() {
+    let i18n = (this.context && this.context.i18n) || this.i18n;
+    return ({ i18n });
+  }
+
+  render() {
+    return this.props.children || null;
+  }
+}
+
+I18nContext.contextTypes = {
+  i18n: PropTypes.object
+};
+I18nContext.childContextTypes = {
+  i18n: PropTypes.object
+};
+I18nContext.propTypes = {
+  locale: PropTypes.string,
+  intlDatas: PropTypes.arrayOf(
+    PropTypes.shape({
+      locale: PropTypes.string,
+      messages: PropTypes.object
+    })
+  ),
+  formatInfos: PropTypes.object,
+  defaultLocale: PropTypes.string
+};
+I18nContext.defaultProps = {
+  locale: defaultProps.locale,
+  intlDatas: defaultProps.intlDatas,
+  formatInfos: defaultProps.formatInfos,
+  defaultLocale: defaultProps.defaultLocale
+};
+
+export default I18nContext;
---
+++ src/client/components/I18Context/index.js
@@ -0,0 +1,1 @@
+export default require('./I18nContext.react').default;
